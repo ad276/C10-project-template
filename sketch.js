e6dac7aeb108e3fe9b4b184bbc72f1e2a9263b31
ad276 <85683804+ad276@users.noreply.github.com>
2021-06-28 03:08:14 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,8 +8,31 @@ var b = 0;
 function setup() {
   createCanvas(400, 400);  
 }
+Canvas.shapecolor=blue;
+btn_red= createButton("RED");
+btn_red.position(100,50);
+btn_red.mousePressed(red_bg);
+
+
+btn_green=createButton("GREEN");
+btn_green=createButton(250,50);
+btn_green.mousePressed(green_bg);
 
 function draw() {
   background(r,g,b);
+
+}
+function red_bg()
+{
+r=255;
+g=0;
+b=0;
 }
 
+
+function green_bg()
+{
+  r=0;
+  g=0;
+  b=0;
+}
